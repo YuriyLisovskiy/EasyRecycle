1226a606029e339fb88af1da587429fd19c818c7
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-03-17 13:27:53 +0200

    added basic UI for login and logout, user account and settings

---
+++ EasyRecycle.UI/src/components/user/settings/Privacy.js
@@ -0,0 +1,66 @@
+import React, {Component} from "react";
+import PasswordVerificationComponent from "./PasswordVerification";
+import PropTypes from "prop-types";
+import ToggleSettingComponent from "./ToggleSetting";
+import UserService from "../../../services/user";
+
+export default class PrivacySettingsComponent extends Component {
+
+	_onToggleShowFullName = (value, handler) => {
+		UserService.editUser(
+			this.props.user.id, null, null, value, null, (data, err) => {
+				if (!err) {
+					let user = this.props.user;
+					user.show_full_name = data.show_full_name;
+					UserService._setCurrentUser(user);
+					handler(data.show_full_name);
+				}
+				else {
+					handler(!value);
+				}
+			}
+		);
+	}
+
+	_onToggleShowRating = (value, handler) => {
+		UserService.editUser(
+			this.props.user.id, null, null, null, value, (data, err) => {
+				if (!err) {
+					let user = this.props.user;
+					user.show_rating = data.show_rating;
+					UserService._setCurrentUser(user);
+					handler(data.show_rating);
+				}
+				else {
+					handler(!value);
+				}
+			}
+		);
+	}
+
+	render() {
+		return <div className="p-3">
+			<div className="row">
+				<div className="col-12 border-bottom mb-4">
+					<small className="text-muted font-weight-bold">PRIVACY</small>
+				</div>
+				<div className="col-12">
+					<ToggleSettingComponent title="Show full name"
+					                        subtitle="Display first and last name on your account page."
+					                        initialValue={this.props.user.show_full_name}
+					                        onToggle={this._onToggleShowFullName}/>
+				</div>
+				<div className="col-12 mt-3">
+					<ToggleSettingComponent title="Show rating"
+					                        subtitle="Display rating on your account page."
+					                        initialValue={this.props.user.show_rating}
+					                        onToggle={this._onToggleShowRating}/>
+				</div>
+			</div>
+		</div>;
+	}
+}
+
+PasswordVerificationComponent.propTypes = {
+	user: PropTypes.object
+}
---
+++ EasyRecycle.UI/src/services/user.js
@@ -0,0 +1,154 @@
+import BaseService from "./base";
+
+class UserService extends BaseService {
+
+	constructor() {
+		super();
+		this._URL_CORE = this._BASE_URL + '/core';
+		this._URL_USERS = this._URL_CORE + '/users';
+		this._URL_ADMIN_USERS = this._URL_CORE + '/admin/users';
+		this._URL_SELF = this._URL_USERS + '/self';
+	}
+
+	// returns:
+	//  {
+	//    "id": <int>,
+	//    "first_name": <string>,
+	//    "last_name": <string>,
+	//    "username": <string>,
+	//    "email": <string>,
+	//    "avatar_link": <string (full url)>,
+	//    "is_superuser": <bool>,
+	//    "is_banned": <bool>,
+	//    "is_garbage_collector": <bool>,
+	//    "rating": <int>,
+	//    "show_full_name": <bool>,
+	//    "show_rating": <bool>
+	//  }
+	getUser = (id, handler) => {
+		return this.get({
+			url: this._URL_USERS + '/' + id.toString()
+		}, handler);
+	}
+
+	// returns:
+	//  {
+	//    "id": <int>,
+	//    "first_name": <string>,
+	//    "last_name": <string>,
+	//    "username": <string>,
+	//    "email": <string>,
+	//    "avatar_link": <string (full url)>,
+	//    "is_superuser": <bool>,
+	//    "is_garbage_collector": <bool>,
+	//    "rating": <int>,
+	//    "show_full_name": <bool>,
+	//    "show_rating": <bool>
+	//  }
+	getMe = (handler) => {
+		return this.get({url: this._URL_SELF}, handler);
+	}
+
+	// returns:
+	//  {
+	//    "first_name": <string>,
+	//    "last_name": <string>,
+	//    "avatar_link": <string> (full url),
+	//    "show_full_name": <bool>,
+	//    "show_rating": <bool>
+	//  }
+	editUser = (id, firstName, lastName, showFullName, showRating, handler) => {
+		let data = {};
+		if (firstName !== null) {
+			data['first_name'] = firstName;
+		}
+
+		if (lastName !== null) {
+			data['last_name'] = lastName;
+		}
+
+		if (showFullName !== null) {
+			data['show_full_name'] = showFullName;
+		}
+
+		if (showRating !== null) {
+			data['show_rating'] = showRating;
+		}
+
+		this.put({
+			url: this._URL_SELF + '/edit',
+			data: data
+		}, handler);
+	}
+
+	// returns:
+	//  {
+	//    "first_name": <string>,
+	//    "last_name": <string>,
+	//    "avatar_link": <string> (full url),
+	//    "show_full_name": <bool>
+	//  }
+	updateAvatar = (id, avatar, handler) => {
+		let formData = new FormData();
+		if (avatar) {
+			formData.append('avatar', avatar);
+		}
+
+		this.put({
+			url: this._URL_SELF + '/edit/avatar',
+			data: formData,
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		}, handler);
+	}
+
+	editEmail = (id, email, password, handler) => {
+		this.put({
+			url: this._URL_SELF + '/edit/email',
+			data: {
+				email: email,
+				password: password
+			}
+		}, handler);
+	}
+
+	editPassword = (id, oldPassword, newPassword, handler) => {
+		this.put({
+			url: this._URL_SELF + '/edit/password',
+			data: {
+				old_password: oldPassword,
+				new_password: newPassword
+			}
+		}, handler);
+	}
+
+	deactivateMe = (id, password, handler) => {
+		this.put({
+			url: this._URL_SELF + '/deactivate',
+			data: {
+				password: password
+			}
+		}, handler);
+	}
+
+	// Administration privileges are required.
+	// returns:
+	// {}
+	banUser = (userId, handler) => {
+		this.put({
+			url: this._URL_ADMIN_USERS + '/' + userId.toString() + '/ban'
+		}, handler);
+	}
+
+	// Administration privileges are required.
+	// returns:
+	// {}
+	unbanUser = (userId, handler) => {
+		this.put({
+			url: this._URL_ADMIN_USERS + '/' + userId.toString() + '/unban'
+		}, handler);
+	}
+}
+
+export default new UserService();
