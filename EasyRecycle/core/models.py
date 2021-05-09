ebaa33aa26d39d8821f16e6781e82ab9a8214b4e
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2021-05-09 17:39:54 +0300

    fixed len of image field

--- EasyRecycle/core/models.py
+++ EasyRecycle/core/models.py
@@ -6,6 +6,6 @@ class UserModel(AbstractUser):
 	is_banned = models.BooleanField(default=False)
 	is_garbage_collector = models.BooleanField(default=False)
 	is_commercial = models.BooleanField(default=False)
-	avatar_info = models.CharField(max_length=32)
+	avatar_info = models.CharField(max_length=50)
 	rating = models.IntegerField(default=0)
 	show_full_name = models.BooleanField(default=True)
